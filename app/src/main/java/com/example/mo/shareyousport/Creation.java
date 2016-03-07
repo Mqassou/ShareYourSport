ecd10600b5d8bd0fc3ac80aaa3df10358843dacb
Sokih <maximilien.petit1@gmail.com>
2016-03-07 22:26:21 +0100

    TestCommitMax

--- app/src/main/java/com/example/mo/shareyousport/Creation.java
+++ app/src/main/java/com/example/mo/shareyousport/Creation.java
@@ -17,6 +17,7 @@ public class Creation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //TestCommitMax
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creation);
 
