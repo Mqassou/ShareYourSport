e276b6ae16f47cede187c3909410d4dad8a61a84
Mohamed QASSOU <mohamed.qassou@efrei.net>
2016-03-14 23:45:29 +0100

    Mise en place du système d'authentification avec le server

--- app/src/main/java/com/example/mo/shareyousport/Login.java
+++ app/src/main/java/com/example/mo/shareyousport/Login.java
@@ -1,6 +1,11 @@
 package com.example.mo.shareyousport;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,15 +21,18 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedWriter;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
-import java.util.concurrent.Executors;
+
 
 import butterknife.ButterKnife;
 import butterknife.Bind;
@@ -32,11 +40,16 @@ import butterknife.Bind;
 public class Login extends AppCompatActivity {
     private static final String TAG = "Login";
     private static final int REQUEST_SIGNUP = 0;
+    private String result_login = "false";
 
-    @Bind(R.id.input_email) EditText _emailText;
-    @Bind(R.id.input_password) EditText _passwordText;
-   @Bind(R.id.btn_login) Button _loginButton;
-   @Bind(R.id.link_signup) TextView _signupLink;
+    @Bind(R.id.input_email)
+    EditText _emailText;
+    @Bind(R.id.input_password)
+    EditText _passwordText;
+    @Bind(R.id.btn_login)
+    Button _loginButton;
+    @Bind(R.id.link_signup)
+    TextView _signupLink;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,32 +79,26 @@ public class Login extends AppCompatActivity {
     public void login() {
         Log.d(TAG, "Login");
 
-        if (!validate()) {
-            onLoginFailed();
-            return;
-        }
-
-        _loginButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(Login.this,R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
-
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
 
-        // TODO: Implement your own authentication logic here.
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            return;
+        } else {
+            _emailText.setError(null);
+        }
 
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        //progressDialog.dismiss();
-                    }
-                }, 3000);
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            return;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        // Envoie de la requête http avec la method post à la base de données
+        PostClass requeteHttp = new PostClass();
+        requeteHttp.execute(email, password);
     }
 
 
@@ -119,136 +126,122 @@ public class Login extends AppCompatActivity {
     }
 
     public void onLoginFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
         _loginButton.setEnabled(true);
     }
 
-    public boolean validate() {
-        boolean valid = true;
-        String result = "";
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
+    // Classe qui contient 3 méthodes pour pouvoir effectuer une requete http
+    // Ces requêtes nécessitent d'être effectuer dans un  thread
+    private class PostClass extends AsyncTask<String, Void, String> {
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
-            valid = false;
-        } else {
-            _emailText.setError(null);
+        final ProgressDialog progressDialog = new ProgressDialog(Login.this, R.style.AppTheme_Dark_Dialog);
+
+        @Override //Cette méthode s'execute en premier, elle ouvre une simple boite de dialogue
+        protected void onPreExecute() {
+            progressDialog.setIndeterminate(true);
+            progressDialog.setMessage("Authenticating...");
+            progressDialog.show();
         }
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
-        } else {
-            _passwordText.setError(null);
-        }
+        @Override//Cette méthode s'execute en deuxième
+        protected String doInBackground(String... params) {
 
-        // Envoie de la requête à la base de données
-/* 
-        try {
-            URL url = new URL("http://humanapp.assos.efrei.fr/shareyoursport/script/script.php");
-            final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("OBJET", "login");
-            connection.setRequestProperty("EMAIL", _emailText.getText().toString());
-            connection.setRequestProperty("PASSWORD", _passwordText.getText().toString());
-
-            connection.setDoOutput(true);
-
-
-            Executors.newSingleThreadExecutor().execute(new Runnable() {
-                @Override
-                public void run() {
+            ConnectivityManager check = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo[] info = check.getAllNetworkInfo();
+            for (int i = 0; i < info.length; i++) {
+                if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                    String result;
 
                     try {
-                        DataOutputStream dStream = new DataOutputStream(connection.getOutputStream());
-                        dStream.flush();
-                        dStream.close();
+                        /////////////////////////////// REQUETE HTTP /////////////////////
+                        URL url = new URL("http://humanapp.assos.efrei.fr/shareyoursport/script/script.php");
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        connection.setConnectTimeout(3000);
+                        connection.setRequestMethod("POST");
+                        connection.setDoInput(true);
+                        connection.setDoOutput(true);
+
+                        /// Mise en place des differents parametre necessaire ////
+
+                        Uri.Builder builder = new Uri.Builder()
+                                .appendQueryParameter("OBJET", "login")
+                                .appendQueryParameter("EMAIL", params[0]) // params[0] entree en parametre dans la method login (cf:  requeteHttp.execute(email, password);)
+                                .appendQueryParameter("PASSWORD", params[1]); //idem
+                        String query = builder.build().getEncodedQuery();
+
+                        OutputStream os = connection.getOutputStream();
+                        BufferedWriter writer = new BufferedWriter(
+                                new OutputStreamWriter(os, "UTF-8"));
+                        writer.write(query);
+                        writer.flush();
+                        writer.close();
+                        os.close();
+
+                        connection.connect();
+
+                        ///////////////////////////////BUFFERREADER/////////////////////
+
+                        Reader reader =new InputStreamReader(connection.getInputStream(), "UTF-8");
+                        char[] buffer = new char[50];
+                        reader.read(buffer);  /// On recupere ce que nous a envoyés le fichier php
+                        result = new String(buffer);
+                        reader.close();
+
+
+                        //////////////////////JSON////////////////////////////////////
+                        try {
+
+                            JSONObject object = new JSONObject(result);
+                            connection.disconnect();
+                            return object.getString("value"); // On retourne true ou false
+
+
+                        } catch (JSONException e) {
+                            Log.e("log_tag", "Error parsing data " + e.toString());
+
+                        }
+
+                        ///////////////// Code permettant de vérifier la connexion avecle server////////////////
+                  /*      if (connection.getResponseCode() == 200) {
+                            return   String.valueOf(connection.getResponseCode()) + " "+ connection.getResponseMessage();
+                        }
+
+                        return    String.valueOf(connection.getResponseCode()) + " "+ connection.getResponseMessage();
+                    */
                     } catch (MalformedURLException e) {
-                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (ProtocolException e) {
                         e.printStackTrace();
                     } catch (IOException e) {
-                        // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
 
                 }
-            });
-
-
-
-           Executors.newSingleThreadExecutor().execute(new Runnable() {
-                @Override
-                public void run() {
-
-                    try {
-                        BufferedReader br;
-                         br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    } catch (MalformedURLException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-
-                }
-            });
-
-
-
-
-            String line = "";
-            StringBuilder responseOutput = new StringBuilder();
-            System.out.println("output===============" + br);
-            while((line = br.readLine()) != null ) {
-                responseOutput.append(line);
             }
-            br.close();
-            result = responseOutput.toString();
-        }
-        catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+
+            return "false";
+
         }
 
+        @Override // La troisème méthode qui s'execute en dernier
+                   // String th, est la valeur que nous a retournee doInBackground
+        protected void onPostExecute(String th) {
+            progressDialog.dismiss();
+
+                if (Boolean.parseBoolean(th)) {
+                    Intent myIntent = new Intent(Login.this, Creation.class);
+                    startActivity(myIntent);
 
 
-
-
-
-
-        try{
-            JSONArray jArray = new JSONArray(result);
-            for(int i=0;i<jArray.length();i++){
-                JSONObject json_data = jArray.getJSONObject(i);
-                Log.i("log_tag","value: "+json_data.getString("value"));
-
-
-                if(json_data.getString("value").equals("true"))
-                {
-                    Toast.makeText(getBaseContext(), "Connexion à la BDD réussie", Toast.LENGTH_LONG).show();
-
+                } else {
+                    Toast.makeText(getBaseContext(), "Erreur de connexion ", Toast.LENGTH_LONG).show();
                 }
-                else
-                {
-                    Toast.makeText(getBaseContext(), "Echec de la connexion à la BDD ", Toast.LENGTH_LONG).show();
-                }
-            }
+
+
+
         }
-    catch(JSONException e){
-        Log.e("log_tag", "Error parsing data "+e.toString());
+
     }
 
 
-*/
-
-        return valid;
-    }
 }
