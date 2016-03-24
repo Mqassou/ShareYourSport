8edc7043c3ab8544979b7e7dd090d554ed12b582
Mohamed QASSOU <mohamed.qassou@efrei.net>
2016-03-24 23:03:19 +0100

    Mise en place des boutons pour augmenter ou diminuer le nombre de personnes.
    Ajout d'une liste de sport s'affichant sous forme de pop up
    
    Refonte du layout de l'activité création

--- app/src/main/java/com/example/mo/shareyousport/Creation.java
+++ app/src/main/java/com/example/mo/shareyousport/Creation.java
@@ -1,5 +1,7 @@
 package com.example.mo.shareyousport;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,14 +10,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Creation extends AppCompatActivity {
 
+    private Button choixDusport;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //TestCommitMax2
@@ -23,29 +28,57 @@ public class Creation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creation);
 
+        ////////////////////  Boutton liste des sports ///////////////////////////////////
+        choixDusport= (Button) findViewById(R.id.Liste_sport);
+
+        choixDusport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowAlertDialogWithListview();
+
+            }
+        });
+        ////////////////////  Fin Boutton liste des sports ///////////////////////////////////
+
+        ////////////////////  Choix du nombre de personnes ///////////////////////////////////
+
+        ImageView augmenter = (ImageView) findViewById(R.id.plus);
+        ImageView diminuer = (ImageView) findViewById(R.id.minus);
+        final EditText nombrepersonne = (EditText) findViewById(R.id.nombrepersonne);
+
+        augmenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               String nombre= nombrepersonne.getText().toString();
+                int nbr=Integer.parseInt(nombre);
+                nbr++;
+                nombre=Integer.toString(nbr);
+                nombrepersonne.setText(nombre);
+            }
+        });
+
+        diminuer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nombre= nombrepersonne.getText().toString();
+                int nbr=Integer.parseInt(nombre);
+                if(nbr>0) {
+                    nbr--;
+                }
+                nombre=Integer.toString(nbr);
+                nombrepersonne.setText(nombre);
+            }
+        });
+
+        ////////////////////  fin Choix du nombre de personnes ///////////////////////////////////
+
+
+        ////////////////////  Géolocation ///////////////////////////////////
         String textPosition = thisIntent.getStringExtra("donnée");
 
-        ListView listeSport = (ListView) findViewById(R.id.Liste_sport);
-
-        ArrayList<String> listeItem = new ArrayList<>();
-        listeItem.add(getString(R.string.Football));
-        listeItem.add(getString(R.string.Rugby));
-        listeItem.add(getString(R.string.Basketball));
-        listeItem.add(getString(R.string.Tennis));
-        listeItem.add(getString(R.string.Badminton));
-        listeItem.add(getString(R.string.Volley_ball));
-        listeItem.add(getString(R.string.Handball));
-        listeItem.add(getString(R.string.Course));
-        listeItem.add(getString(R.string.Cyclisme));
-
-        ArrayAdapter<String> itemsAdapter =new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, listeItem);
-        listeSport.setAdapter(itemsAdapter);
-
-
-
         EditText txt = (EditText) findViewById(R.id.editText);
-
-
 
         if(textPosition != null){
             txt.setText(textPosition);
@@ -58,10 +91,7 @@ public class Creation extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
-
-
-
-
+        //////////////////// Fin Géolocation ///////////////////////////////////
 
         ImageView img = (ImageView) findViewById(R.id.back);
         img.setOnClickListener(new View.OnClickListener() {
@@ -72,8 +102,34 @@ public class Creation extends AppCompatActivity {
             }
         });
 
+    }
+    public void ShowAlertDialogWithListview()
+    {
 
+        ArrayList<String> listeSport = new ArrayList<>();
+        listeSport.add(getString(R.string.Football));
+        listeSport.add(getString(R.string.Rugby));
+        listeSport.add(getString(R.string.Basketball));
+        listeSport.add(getString(R.string.Tennis));
+        listeSport.add(getString(R.string.Badminton));
+        listeSport.add(getString(R.string.Volley_ball));
+        listeSport.add(getString(R.string.Handball));
+        listeSport.add(getString(R.string.Course));
+        listeSport.add(getString(R.string.Cyclisme));
 
+        final CharSequence[] dialogSport = listeSport.toArray(new String[listeSport.size()]);
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Sport");
+        dialogBuilder.setItems(dialogSport, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                String selectedText = dialogSport[item].toString();  //Selected item in listview
+                choixDusport.setText(selectedText);
+            }
+        });
+        //Create alert dialog object via builder
+        AlertDialog alertDialogObject = dialogBuilder.create();
+        //Show the dialog
+        alertDialogObject.show();
     }
 
 }
